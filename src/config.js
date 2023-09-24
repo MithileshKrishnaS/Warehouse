b397ec488a7ed695564a2ed18b65d7d9ba4ec8a9
MithileshKrishnaS <mithileshkrishna20@gmail.com>
2023-09-24 17:31:50 +0530

    add config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    API     :'http://127.0.0.1:4000/',
+    API     :'https://warehouse-node.onrender.com/',
     headers : {
         "Content-Type": "application/json",
         "Authorization" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
